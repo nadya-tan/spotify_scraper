c9531c10a5a51e1f8edbe5083048eb4401f43439
ntan0033 <ntan0033@student.monash.edu>
2023-08-29 20:59:08 +0800

    Added Album, Subgenre, Popularity
    Added scraping + data for album info, subgenres associated with artist, and track popularity

--- main.py
+++ main.py
@@ -32,6 +32,10 @@ def main():
     playlist_tracks_titles = []
     playlist_tracks_artists = []
     playlist_tracks_first_artists = []
+    playlist_tracks_album_id = []
+    playlist_tracks_album_title = []
+    playlist_tracks_album_genres = []
+    playlist_tracks_popularity = []
 
     for track in playlist_tracks_data['items']:
         playlist_tracks_id.append(track['track']['id'])
@@ -41,8 +45,15 @@ def main():
         for artist in track['track']['artists']:
             artist_list.append(artist['name'])
         playlist_tracks_artists.append(artist_list)
+        first_artist_id = track['track']['artists'][0]['id']
         playlist_tracks_first_artists.append(artist_list[0])
-
+        # get album info
+        playlist_tracks_album_id.append(track['track']['album']['id'])
+        playlist_tracks_album_title.append(track['track']['album']['name'])
+        # get genre
+        playlist_tracks_album_genres.append(spotify.artist(first_artist_id)['genres'])
+        # get track popularity
+        playlist_tracks_popularity.append(track['track']['popularity'])
     # get features
     features = spotify.audio_features(playlist_tracks_id)
     features_df = pd.DataFrame(data=features, columns=features[0].keys())
@@ -51,13 +62,18 @@ def main():
     features_df['title'] = playlist_tracks_titles
     features_df['first_artist'] = playlist_tracks_first_artists
     features_df['all_artists'] = playlist_tracks_artists
+    features_df['album_id'] = playlist_tracks_album_id
+    features_df['album_name'] = playlist_tracks_album_title
+    features_df['genres'] = playlist_tracks_album_genres
+    features_df['popularity'] = playlist_tracks_popularity
     print(features_df)
     #features_df = features_df.set_index('id')
     features_df = features_df[['id', 'title', 'first_artist', 'all_artists',
+                               'album_id', 'album_name', 'genres',
                                'danceability', 'energy', 'key', 'loudness',
                                'mode', 'acousticness', 'instrumentalness',
                                'liveness', 'valence', 'tempo',
-                               'duration_ms', 'time_signature']]
+                               'duration_ms', 'time_signature', 'popularity']]
     features_df.tail()
 
     # convert to csv
